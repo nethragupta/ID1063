5de37f275f0bb47065b55e0d71b2e08f9bdc36d0
Nethra Gupta <nethragupta1@gmail.com>
2026-09-09 16:22:04 +0530

    Changed code

--- itp-6-5.c
+++ itp-6-5.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
-int main()
-{
-	char a[50];
-	printf("Enter a word:");
-	scanf("%s", a);
-	int length = strlen(a);
-	int t=0;
-	for (int i = 0; i < length; i++)
-		if (a[i] != a[length-1-i])
-		{t = 1;
-			break;
-		}
-if (t==0)
-	printf("Palindrome");
-else
-printf("Not a Palindrome");
-			
-return 0;
+#include <string.h>
+
+int main() {
+    char a[50];
+    printf("Enter a phrase: ");
+    fgets(a, sizeof(a), stdin); 
+    a[strcspn(a, "\n")] = 0;
+
+    int length = strlen(a);
+    int t = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (a[i] != a[length - 1 - i]) {
+            t = 1;
+            break;
+        }
+    }
+
+    if (t == 0)
+        printf("Palindrome\n");
+    else
+        printf("Not a Palindrome\n");
+
+    return 0;
 }
+
