989ee4b56a033e7c5cba82d1ebd617a10d8b9637
Nethra Gupta <nethragupta1@gmail.com>
2026-09-23 16:35:45 +0530

    Add file

---
+++ lab9_4/q4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+// returns first i where |a[i+1]-a[i]| <= tol, else -1
+int firstStable(double a[], int n, double tol)
+{
+    int i;
+    for (i = 0; i < n-1; i++)
+        if (fabs(a[i+1]-a[i]) <= tol)   // difference small enough
+            return i;
+    return -1;   // never got within tolerance
+}
+
+int main()
+{
+    int n, i;
+    double tol;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    double a[n];
+    printf("Enter the readings: ");
+    for (i = 0; i < n; i++)
+        scanf("%lf", &a[i]);
+    printf("Enter tolerance: ");
+    scanf("%lf", &tol);
+    printf("%d\n", firstStable(a, n, tol));
+    return 0;
+}
