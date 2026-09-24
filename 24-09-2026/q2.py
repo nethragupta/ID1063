29cc8f2a0b4c310b6919321a9901e4b7a075e4b3
Nethra Gupta <nethragupta1@gmail.com>
2026-09-24 17:32:03 +0530

    Add file

--- 24-09-2026/q2.py
+++ 24-09-2026/q2.py
@@ -1,2 +1,23 @@
-print("Hello world")
+import numpy as np
+
+P=np.array([[1,0,1],[0,1,0],[1,0,1]])
+
+ev = np.linalg.eigvals(P)
+ev=np.round(ev.real,4)
+print("eigenvalues:",sorted(ev))
+
+# A
+tr=np.trace(P)
+print("A :",tr,"vs",sum(ev),"->",np.isclose(tr,sum(ev)))
+
+# B
+pp = P.T@P
+print("B :",np.array_equal(pp,np.eye(3)))
+print(pp)
+
+# C
+print("C :",np.array_equal(P.T,-P))
+
+# D
+print("D :",np.allclose(abs(ev),1))
 
